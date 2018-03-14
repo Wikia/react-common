ee50e3327e67ddb9d9595f4b2966602fd4301de0
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2018-03-14 16:58:33 -0700

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// UI
+export {default as ExpandableText} from './components/ExpandableText';
+export {default as Loader} from './components/Loader';
+
+// Icons
+export {default as VideoPlayIcon} from './components/VideoPlayIcon';
