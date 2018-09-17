f370c5326912c642f4a86b702ed6f90608c209e3
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2018-09-16 18:26:59 -0700

    cleanup

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 import uniqueId from 'lodash.uniqueid';
 
 import './styles.scss';
