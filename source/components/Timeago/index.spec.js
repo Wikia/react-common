7c3b5e570897fd6f47ae5a073f5b4ed699d28587
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2018-11-18 16:17:31 -0800

    make test better

--- source/components/Timeago/index.spec.js
+++ source/components/Timeago/index.spec.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import Timeago from './index';
 
 test('Renders Timeago component', () => {
-    const datetime = 'Tue Jul 17 2018 23:58:43 GMT+0000';
+    const datetime = 'Tue Jul 17 2015 23:58:43 GMT+0000';
     const component = shallow(<Timeago datetime={datetime} />);
 
     expect(component).toMatchSnapshot();
