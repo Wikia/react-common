a559a6ea7211c25223547370aeb78712b522e7f2
nmonterroso <nmonterroso@users.noreply.github.com>
2018-11-19 11:54:04 -0800

    select input basics

---
+++ source/components/Select/README.md
@@ -0,0 +1,11 @@
+Basic usage:
+```js
+<Select
+    options={[
+        // use createOption(val, label) for these
+        {value: 1, label: "label 1"},
+        {value: 2, label: "label 2"},
+        {value: 3, label: "label 3"},
+    ]}
+/>
+```
---
+++ source/components/Select/index.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactSelect from 'react-select';
+
+export function createOption(value, label) {
+    return { value, label };
+}
+
+function callWithValues(func, values, isMulti) {
+    if (!func) {
+        return;
+    }
+
+    if (isMulti) {
+        func(values);
+        return;
+    }
+
+    if (values.length === 0) {
+        func(null, null);
+    }
+
+    const { value, label } = values[0];
+    func(value, label);
+}
+
+/**
+ * A select input. For post-interaction events the signature is `onX(value, label)` for single-value selects, and
+ * `onX([{value, label}, ...])` for multi-select
+ */
+class Select extends React.Component {
+    constructor(props) {
+        super(props);
+        this.selectRef = React.createRef();
+        this.onRequestNoOptionsMessage = this.onRequestNoOptionsMessage.bind(this);
+        this.onBlur = this.onBlur.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onFocus = this.onFocus.bind(this);
+        this.onTextInputChange = this.onTextInputChange.bind(this);
+    }
+
+    onBlur() {
+        if (!this.props.onBlur || !this.selectRef.current) {
+            return;
+        }
+
+        callWithValues(this.props.onBlur, this.selectRef.current.select.getCommonProps().getValue(), this.props.multi);
+    }
+
+    onChange(values) {
+        callWithValues(this.props.onChange, values, this.props.multi);
+    }
+
+    onFocus() {
+        if (!this.props.onFocus) {
+            return;
+        }
+
+        this.props.onFocus();
+    }
+
+    onRequestNoOptionsMessage({ inputValue }) {
+        const { onNoOptions } = this.props;
+        if (typeof onNoOptions === 'function') {
+            return onNoOptions(inputValue);
+        }
+
+        return onNoOptions;
+    }
+
+    onTextInputChange(input) {
+        if (this.props.onTextInputChange) {
+            this.props.onTextInputChange(input);
+        }
+    }
+
+    getRootClassName() {
+        return ['fandom-select', this.props.className]
+            .filter(Boolean)
+            .join(' ');
+    }
+
+    render() {
+        return (
+            <ReactSelect
+                ref={this.selectRef}
+                autoFocus={this.props.autoFocus}
+                className={this.getRootClassName()}
+                classNamePrefix="fandom-select"
+                isDisabled={this.props.disabled || this.props.loading}
+                isLoading={this.props.loading}
+                isMulti={this.props.multi}
+                isSearchable={this.props.searchable}
+                noOptionsMessage={this.onRequestNoOptionsMessage}
+                placeholder={this.props.placeholder}
+                onBlur={this.onBlur}
+                onChange={this.onChange}
+                onFocus={this.onFocus}
+                onInputChange={this.onTextInputChange}
+                options={this.props.options}
+            />
+        );
+    }
+}
+
+Select.propTypes = {
+    /**
+     * Focus the control when it is mounted
+     */
+    autoFocus: PropTypes.bool,
+    /**
+     * Additional class for root element
+     */
+    className: PropTypes.string,
+    /**
+     * whether or not the input is disabled
+     */
+    disabled: PropTypes.bool,
+    /**
+     * whether or not to show loading indicator
+     */
+    loading: PropTypes.bool,
+    /**
+     * whether or not multiple values are allowed
+     */
+    multi: PropTypes.bool,
+    /**
+     * called when the input is blurred `onBlur(val, label)`
+     */
+    onBlur: PropTypes.func,
+    /**
+     * called when the input is changed
+     */
+    onChange: PropTypes.func,
+    /**
+     * called when input is focused `onFocus()`
+     */
+    onFocus: PropTypes.func,
+    /**
+     * message when no options are present. If a `func`, the only argument is the input to the text input
+     */
+    onNoOptions: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.func,
+    ]),
+    /**
+     * fired when the search input is changed. `onTextInputChange(currentInput)`
+     */
+    onTextInputChange: PropTypes.func,
+    /**
+     * options to display. Use `createOption` exported from this module to create options
+     */
+    options: PropTypes.arrayOf(PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        value: PropTypes.any.isRequired,
+    })).isRequired,
+    /**
+     * Placeholder text used when no value is selected
+     */
+    placeholder: PropTypes.string,
+    /**
+     * whether or not to allow text searching
+     */
+    searchable: PropTypes.bool,
+};
+
+Select.defaultProps = {
+    autoFocus: false,
+    className: '',
+    disabled: false,
+    loading: false,
+    multi: false,
+    onBlur: undefined,
+    onChange: undefined,
+    onFocus: undefined,
+    onNoOptions: 'No Options',
+    onTextInputChange: undefined,
+    placeholder: 'Select...',
+    searchable: true,
+};
+
+export default Select;
