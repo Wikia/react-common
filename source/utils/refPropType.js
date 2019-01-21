4633f7b51685b4152e352c38560b2ab49f6ed717
Karol Tatała <ktatala@wikia-inc.com>
2019-01-21 11:24:50 +0100

    IW-1296 | update refPropType

--- source/utils/refPropType.js
+++ source/utils/refPropType.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
 // to make it work with SSR
-const Element = typeof global.Element === 'undefined' ? () => {} : global.Element;
+const Element = typeof global.Element === 'undefined' ? null : global.Element;
 
 export default PropTypes.shape({ current: PropTypes.instanceOf(Element) });
