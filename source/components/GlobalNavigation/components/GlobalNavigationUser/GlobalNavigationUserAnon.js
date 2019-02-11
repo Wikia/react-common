f00c1a882d4b288d50134b0f7611b69b4e296bea
xkxd <krzysztof.derek@gmail.com>
2019-02-11 16:27:50 +0100

    IW-1022 | anon dropdown

--- source/components/GlobalNavigation/components/GlobalNavigationUser/GlobalNavigationUserAnon.js
+++ source/components/GlobalNavigation/components/GlobalNavigationUser/GlobalNavigationUserAnon.js
@@ -1,16 +1,59 @@
 import React from 'react';
+import { withTranslation } from 'react-i18next';
+
 import Dropdown from '../../../Dropdown';
 import Avatar from '../../../Avatar';
+import Button from "../../../Button";
 
 class GlobalNavigationUserAnon extends React.Component {
-    render() {
+    renderToggle = (chevron) => {
         return (
-            <Dropdown toggle={(chevron) => (<React.Fragment><Avatar />{chevron}</React.Fragment>)}
-                      className="wds-global-navigation__user-menu wds-global-navigation__user-anon">
-                Content
+            <React.Fragment>
+                <Avatar />{chevron}
+            </React.Fragment>
+        );
+    };
+
+    render() {
+        const { data, t } = this.props;
+
+        return (
+            <Dropdown
+                toggle={this.renderToggle}
+                className="wds-global-navigation__user-menu wds-global-navigation__user-anon"
+                contentScrollable={false}
+                dropdownRightAligned={true}
+                contentClassName="wds-global-navigation__dropdown-content"
+            >
+                <ul className="wds-list wds-has-lines-between">
+                    <li>
+                        <Button
+                            fullwidth
+                            href={data.signin.href}
+                            rel="nofollow"
+                            data-tracking-label="account.sign-in"
+                        >
+                            {t(data.signin.title.key)}
+                        </Button>
+                    </li>
+                    <li>
+                        <div className="wds-global-navigation__user-menu-dropdown-caption">
+                            Don't have an account?
+                        </div>
+                        <Button
+                            href={data.register.href}
+                            rel="nofollow"
+                            data-tracking-label="account.register"
+                            fullwidth
+                            secondary
+                        >
+                            {t(data.register.title.key)}
+                        </Button>
+                    </li>
+                </ul>
             </Dropdown>
         );
     }
 }
 
-export default GlobalNavigationUserAnon;
+export default withTranslation()(GlobalNavigationUserAnon);
--- source/components/GlobalNavigation/index.js
+++ source/components/GlobalNavigation/index.js
@@ -14,9 +14,9 @@ class GlobalNavigation extends React.Component {
     renderMainNavigation(navigation) {
         return navigation.map(link => {
             if (link.type === 'link-text') {
-                return <GlobalNavigationLinkText link={link} isStandaloneLink />;
+                return <GlobalNavigationLinkText key={link.href} link={link} isStandaloneLink />;
             } else if (link.type === 'link-group') {
-                return <GlobalNavigationLinkGroup link={link} />
+                return <GlobalNavigationLinkGroup key={link.href} link={link} />
             }
         });
     }
@@ -41,7 +41,7 @@ class GlobalNavigation extends React.Component {
                 <div className="wds-global-navigation__content-bar-right">
                     <div className="wds-global-navigation__dropdown-controls">
                         <GlobalNavigationSearch />
-                        <GlobalNavigationUserAnon />
+                        <GlobalNavigationUserAnon data={model.anon} />
                         <div className="wds-global-navigation__start-a-wiki">
                             <GlobalNavigationLinkButton link={model['create-wiki']} />
                         </div>
