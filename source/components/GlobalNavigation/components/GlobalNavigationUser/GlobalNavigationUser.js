e8d4cc52b1a29672970199079e3c219eeac8f433
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-02-13 11:36:20 +0100

    IW-1022: convert component to function

--- source/components/GlobalNavigation/components/GlobalNavigationUser/GlobalNavigationUser.js
+++ source/components/GlobalNavigation/components/GlobalNavigationUser/GlobalNavigationUser.js
@@ -3,18 +3,16 @@ import React from 'react';
 import GlobalNavigationUserLoggedIn from './GlobalNavigationUserLoggedIn';
 import GlobalNavigationUserAnon from './GlobalNavigationUserAnon';
 
-class GlobalNavigationUser extends React.Component {
-    render() {
-        const { data } = this.props;
-
-        if (data.user) {
-            return <GlobalNavigationUserLoggedIn data={data.user}/>
-        }
-
-        if (data.anon) {
-            return <GlobalNavigationUserAnon data={data.anon}/>
-        }
+const GlobalNavigationUser = ({ data }) => {
+    if (data.user) {
+        return <GlobalNavigationUserLoggedIn data={data.user}/>
     }
-}
+
+    if (data.anon) {
+        return <GlobalNavigationUserAnon data={data.anon}/>
+    }
+
+    return null;
+};
 
 export default GlobalNavigationUser;
