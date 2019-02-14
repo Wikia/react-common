11b0204b06ebfa89e157062e32c2d301d6fc57f1
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-02-14 09:55:52 +0100

    IW-1022: user modal without notifications

--- source/components/GlobalNavigation/components/GlobalNavigationUser/GlobalNavigationMobileLoggedIn.js
+++ source/components/GlobalNavigation/components/GlobalNavigationUser/GlobalNavigationMobileLoggedIn.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import Button from "../../../Button";
 import Avatar from "../../../Avatar";
+import GlobalNavigationUserModal from "./GlobalNavigationUserModal";
 
-const GlobalNavigationMobileLoggedIn = ({ data }) => {
+const GlobalNavigationMobileLoggedIn = ({ user, openModal, modalOpen }) => {
     return (
-        <Button onClick={() => {}} text className="wds-global-navigation__modal-control wds-global-navigation__modal-control-user">
-            <Avatar src={data.avatar_url} alt={data.username} />
-
-                {/* TODO: notification counter only if there are notifications */}
-            <span className="wds-global-navigation__avatar-notifications-counter">
-                {/*{{wdsOnSiteNotifications.unreadCountWithLimit}}*/}
-                3
-            </span>
-        </Button>
-    // {{global-navigation/user-modal
-    //     isOpen=isUserModalOpen
-    //     user=model.user
-    //     header=model.notifications.header.title.key
-    //     track=track
-    // }}
+        <React.Fragment>
+            <Button onClick={() => openModal('user')} text className="wds-global-navigation__modal-control wds-global-navigation__modal-control-user">
+                <Avatar src={user.avatar_url} alt={user.username} />
+                    {/* TODO: notification counter only if there are notifications */}
+                <span className="wds-global-navigation__avatar-notifications-counter">
+                    {/*{{wdsOnSiteNotifications.unreadCountWithLimit}}*/}
+                    3
+                </span>
+            </Button>
+            <GlobalNavigationUserModal user={user} isOpen={modalOpen}/>
+        </React.Fragment>
     )
 };
 
