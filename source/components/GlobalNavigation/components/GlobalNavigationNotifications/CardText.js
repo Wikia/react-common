09b6f3d93180236bc0171471b6174200f68d7b9c
xkxd <krzysztof.derek@gmail.com>
2019-02-19 12:12:03 +0100

    Review

--- source/components/GlobalNavigation/components/GlobalNavigationNotifications/CardText.js
+++ source/components/GlobalNavigation/components/GlobalNavigationNotifications/CardText.js
@@ -10,14 +10,14 @@ import {
     isDiscussionReplyUpvote,
 } from '../../models/notificationTypes';
 
-const getReplyMessageBody = (t, { title, totalUniqueActors, latestActors, postTitleMarkup }) => {
+const getReplyMessageBody = (translateFunc, { title, totalUniqueActors, latestActors, postTitleMarkup }) => {
     const hasTwoUsers = totalUniqueActors === 2;
     const hasThreeOrMoreUsers = totalUniqueActors > 2;
     const firstReplierName = get(latestActors, '[0].name');
 
     if (title) {
         if (hasThreeOrMoreUsers) {
-            return t('notifications-replied-by-multiple-users-with-title', {
+            return translateFunc('notifications-replied-by-multiple-users-with-title', {
                 postTitle: postTitleMarkup,
                 mostRecentUser: firstReplierName,
                 number: totalUniqueActors - 1,
@@ -25,88 +25,88 @@ const getReplyMessageBody = (t, { title, totalUniqueActors, latestActors, postTi
         }
 
         if (hasTwoUsers) {
-            return t('notifications-replied-by-two-users-with-title', {
+            return translateFunc('notifications-replied-by-two-users-with-title', {
                 firstUser: firstReplierName,
-                secondUser: latestActors[1].name,
+                secondUser: get(latestActors, '[1].name'),
                 postTitle: postTitleMarkup,
             });
         }
 
-        return t('notifications-replied-by-with-title', {
+        return translateFunc('notifications-replied-by-with-title', {
             user: firstReplierName,
             postTitle: postTitleMarkup,
         });
     }
 
     if (hasThreeOrMoreUsers) {
-        return t('notifications-replied-by-multiple-users-no-title', {
+        return translateFunc('notifications-replied-by-multiple-users-no-title', {
             mostRecentUser: firstReplierName,
             number: totalUniqueActors - 1,
         });
     }
     if (hasTwoUsers) {
-        return t('notifications-replied-by-two-users-no-title', {
+        return translateFunc('notifications-replied-by-two-users-no-title', {
             firstUser: firstReplierName,
-            secondUser: latestActors[1].name,
+            secondUser: get(latestActors, '[1].name'),
         });
     }
 
-    return t('notifications-replied-by-no-title', {
+    return translateFunc('notifications-replied-by-no-title', {
         user: firstReplierName,
     });
 };
 
-const getPostUpvoteMessageBody = (t, { title, totalUniqueActors, postTitleMarkup }) => {
+const getPostUpvoteMessageBody = (translateFunc, { title, totalUniqueActors, postTitleMarkup }) => {
     const hasMultipleUsers = totalUniqueActors > 1;
 
     if (title) {
         if (hasMultipleUsers) {
-            return t('notifications-post-upvote-multiple-users-with-title', {
+            return translateFunc('notifications-post-upvote-multiple-users-with-title', {
                 postTitle: postTitleMarkup,
                 number: totalUniqueActors,
             });
         }
 
-        return t('notifications-post-upvote-single-user-with-title', {
+        return translateFunc('notifications-post-upvote-single-user-with-title', {
             postTitle: postTitleMarkup,
         });
     }
 
     if (hasMultipleUsers) {
-        return t('notifications-post-upvote-multiple-users-no-title', {
+        return translateFunc('notifications-post-upvote-multiple-users-no-title', {
             number: totalUniqueActors,
         });
     }
 
-    return t('notifications-post-upvote-single-user-no-title');
+    return translateFunc('notifications-post-upvote-single-user-no-title');
 };
 
-const getReplyUpvoteMessageBody = (t, { title, totalUniqueActors, postTitleMarkup }) => {
+const getReplyUpvoteMessageBody = (translateFunc, { title, totalUniqueActors, postTitleMarkup }) => {
     const hasMultipleUsers = totalUniqueActors > 1;
 
     if (title) {
         if (hasMultipleUsers) {
-            return t('notifications-reply-upvote-multiple-users-with-title', {
+            return translateFunc('notifications-reply-upvote-multiple-users-with-title', {
                 postTitle: postTitleMarkup,
                 number: totalUniqueActors - 1,
             });
         }
 
-        return t('notifications-reply-upvote-single-user-with-title', {
+        return translateFunc('notifications-reply-upvote-single-user-with-title', {
             postTitle: postTitleMarkup,
         });
     }
 
     if (hasMultipleUsers) {
-        return t('notifications-reply-upvote-multiple-users-no-title', {
+        return translateFunc('notifications-reply-upvote-multiple-users-no-title', {
             number: totalUniqueActors,
         });
     }
 
-    return t('notifications-reply-upvote-single-user-no-title');
+    return translateFunc('notifications-reply-upvote-single-user-no-title');
 };
 
-const getText = (t, model) => {
+const getText = (translateFunc, model) => {
     const { type, snippet, title, totalUniqueActors, latestActors } = model;
     const postTitleMarkup = `<b>${title}</b>`;
 
@@ -115,15 +115,15 @@ const getText = (t, model) => {
     }
 
     if (isDiscussionReply(type)) {
-        return getReplyMessageBody(t, { title, latestActors, postTitleMarkup, totalUniqueActors });
+        return getReplyMessageBody(translateFunc, { title, latestActors, postTitleMarkup, totalUniqueActors });
     }
 
     if (isDiscussionPostUpvote(type)) {
-        return getPostUpvoteMessageBody(t, { title, postTitleMarkup, totalUniqueActors });
+        return getPostUpvoteMessageBody(translateFunc, { title, postTitleMarkup, totalUniqueActors });
     }
 
     if (isDiscussionReplyUpvote(type)) {
-        return getReplyUpvoteMessageBody(t, { title, postTitleMarkup, totalUniqueActors });
+        return getReplyUpvoteMessageBody(translateFunc, { title, postTitleMarkup, totalUniqueActors });
     }
 
     return null;
