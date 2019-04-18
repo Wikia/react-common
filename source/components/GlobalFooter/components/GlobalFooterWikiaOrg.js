1e03839f4b7e529e3136c457e5a13d16376ef462
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2019-04-18 08:03:37 +0200

    IW-1945: global-footer for wikia org

--- source/components/GlobalFooter/components/GlobalFooterWikiaOrg.js
+++ source/components/GlobalFooter/components/GlobalFooterWikiaOrg.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
 
+import Svg from './Svg';
 
-const GlobalFooterWikiaOrg = () => {
-    return 'WIKIA ORG';
+const GlobalFooterWikiaOrg = ({ t, model }) => {
+    return <React.Fragment>
+        {model.header &&
+            <h2 className="wds-global-footer-wikia-org__header">
+                <a href={model.header.href} title={model.header.title.value}>
+                    <Svg name={model.header['image-data'].name} className="wds-global-footer__header-logo" />
+                </a>
+            </h2>
+        }
+        <ul className="wds-global-footer-wikia-org__links">
+            <li className="wds-global-footer-wikia-org__link">
+                {t('global-footer-site-overview-link-wikia-inc', {year: year})}
+            </li>
+            {model.site_overview.links.map(link =>
+                <li className="wds-global-footer-wikia-org__link">
+                    <a href={link.href} data-tracking-label={link.tracking_label}>
+                        {t(link.title.key)}
+                    </a>
+                </li>
+            )}
+        </ul>
+    </React.Fragment>
 };
 
-export default GlobalFooterWikiaOrg;
+export default withTranslation()(GlobalFooterWikiaOrg);
