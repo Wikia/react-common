ca27896eb40088c1a6d850cf33e5315688d4f584
Chris Schuhmacher <chris@fandom.com>
2019-08-16 15:19:04 -0700

    cake-5067 | Remove setting properties in beforeEach

--- source/utils/environment.spec.js
+++ source/utils/environment.spec.js
@@ -1,28 +1,25 @@
 import { isSandbox } from './environment';
 
 // Unit tests for isSandbox() in multiple environments
-describe('Tests for is Sandbox: ', () => {
+describe('Unit tests for isSandbox() ', () => {
     beforeEach(() => {
         // reset global window properties for
 
         // fepo & f2
         delete global.window.fandom;
-        global.window.fandom = {};
 
         // mediawiki
         delete global.window.wgTransactionContext;
-        global.window.wgTransactionContext = {};
 
         // upstream
         delete global.window.upstream;
-        global.window.upstream = {};
     });
 
-    test('It should return false when window object property is undefined', () => {
+    test('should return false when window object property is undefined', () => {
         expect(isSandbox()).toEqual(false);
     });
 
-    test('It should return false if on production', () => {
+    test('should return false if on production', () => {
         // simulate window object that is not on a sandbox
         global.window.fandom = {
             config: {
@@ -35,7 +32,7 @@ describe('Tests for is Sandbox: ', () => {
         expect(isSandbox()).toEqual(false);
     });
 
-    test('It should return true if sandbox on fepo', () => {
+    test('should return true if sandbox on fepo', () => {
         // simulate fepo's window object with sandbox environment
         global.window.fandom = {
             environment: {
@@ -46,7 +43,7 @@ describe('Tests for is Sandbox: ', () => {
         expect(isSandbox()).toEqual(true);
     });
 
-    test('It should return true if sandbox is on mediawiki', () => {
+    test('should return true if sandbox is on mediawiki', () => {
         // simulate mw's window object with sandbox environment
         global.window.wgTransactionContext = {
             env: 'sandbox',
@@ -55,7 +52,7 @@ describe('Tests for is Sandbox: ', () => {
         expect(isSandbox()).toEqual(true);
     });
 
-    test('It should return true if sandbox is on f2', () => {
+    test('should return true if sandbox is on f2', () => {
         // simulate f2's window object with sandbox environment
         global.window.fandom = {
             config: {
@@ -68,7 +65,7 @@ describe('Tests for is Sandbox: ', () => {
         expect(isSandbox()).toEqual(true);
     });
 
-    test('It should return true if sandbox is on upstream', () => {
+    test('should return true if sandbox is on upstream', () => {
         // simulate upstream's window object with sandbox environment
         global.window.upstream = {
             env: 'sandbox',
