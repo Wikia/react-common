4b95119c06e7a85150b713faa3861d1ea88c7397
Tomek Kasperek <kasperekt@gmail.com>
2020-11-10 10:25:31 +0100

    IW-4803 Build with icons

---
+++ source/assets/LogoGamepediaBook/README.md
@@ -0,0 +1,4 @@
+
+```js
+<LogoGamepediaBook height="80" />
+```
---
+++ source/assets/LogoGamepediaBook/index.js
@@ -0,0 +1,6 @@
+// This file is generated automatically via extract-assets-from-ds.js
+import React from 'react';
+import LogoGamepediaBook from 'design-system/dist/svg/wds-company-logo-gamepedia-book.svg';
+
+/** @component */
+export default props => <LogoGamepediaBook {...props} />;
