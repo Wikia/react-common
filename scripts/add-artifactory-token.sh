dc0b9813719041b317b9ad5bba6f1064a30a956d
Tomek Kasperek <kasperekt@gmail.com>
2021-01-18 09:21:32 +0100

    IW-4970 Debug

--- scripts/add-artifactory-token.sh
+++ scripts/add-artifactory-token.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo '//artifactory.wikia-inc.com/artifactory/api/npm/wikia-npm/:_authToken=${ARTIFACTORY_NPM_TOKEN}' >> .npmrc
-echo '//artifactory.wikia-inc.com/artifactory/api/npm/wikia-npm/:_authToken=${ARTIFACTORY_NPM_TOKEN}' >> .yarnrc
+echo 'token:' $ARTIFACTORY_NPM_TOKEN
+#echo '//artifactory.wikia-inc.com/artifactory/api/npm/wikia-npm/:_authToken=${ARTIFACTORY_NPM_TOKEN}' >> .npmrc
+#echo '//artifactory.wikia-inc.com/artifactory/api/npm/wikia-npm/:_authToken=${ARTIFACTORY_NPM_TOKEN}' >> .yarnrc
 echo 'Success'
