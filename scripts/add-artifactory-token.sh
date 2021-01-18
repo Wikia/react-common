6bfbaabdd282b217b12e614f66bc7f51b49a56c8
Tomek Kasperek <kasperekt@gmail.com>
2021-01-18 09:26:03 +0100

    IW-4970 Remove script for setting token

--- scripts/add-artifactory-token.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'token:' $ARTIFACTORY_NPM_TOKEN
-#echo '//artifactory.wikia-inc.com/artifactory/api/npm/wikia-npm/:_authToken=${ARTIFACTORY_NPM_TOKEN}' >> .npmrc
-#echo '//artifactory.wikia-inc.com/artifactory/api/npm/wikia-npm/:_authToken=${ARTIFACTORY_NPM_TOKEN}' >> .yarnrc
-echo 'Success'
